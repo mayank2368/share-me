0a1c94343fbc0d877419dbc395b9eac83e743fe6
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-01-13 23:22:35 +0100

    added home and lgin components structure and also added some other dependencies for google login, icon etc

---
+++ src/components/Login/login.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import GoogleLogin from "react-google-login";
+import { FcGoogle } from "react-icons/fc";
+
+const Login = () => {
+  return <h1>Login</h1>;
+};
+
+export default Login;
