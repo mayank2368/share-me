64ba4bedd65f0dd0e82db10a504864a0eb1f563b
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-01-13 23:46:34 +0100

    added home and login components structure also some other dependencies

--- src/components/Login/login.component.jsx
+++ src/components/Login/login.component.jsx
@@ -2,9 +2,25 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import GoogleLogin from "react-google-login";
 import { FcGoogle } from "react-icons/fc";
+import shareVideo from "../../assets/share.mp4";
+import logo from "../../assets/logowhite.png";
 
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
